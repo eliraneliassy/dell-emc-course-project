6a3bc9e6604b89bc93ab95b5a2ec016c7338f50f
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-04-11 11:38:36 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dell-emc-course-project';
+
+  item: Item;
+  constructor() {
+    this.item = {
+      title: 'Title',
+      price: 22,
+      imageUrl: 'https://slice-productimage.s3.amazonaws.com/69/df/0a/4d/47/af/43/b7/a8a7c8f181ed2f9b9190d3f7bd9f5ecbdc9b670e5b59095a'
+    };
+  }
+
 }
---
+++ src/app/item.interface.ts
@@ -0,0 +1,6 @@
+export interface Item {
+    title: string;
+    price: number;
+    imageUrl: string;
+
+}
