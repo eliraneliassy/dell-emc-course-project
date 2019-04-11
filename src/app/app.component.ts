6e3d78b5f65eb44cf7d75eff0f8f4ab352efd956
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-04-11 12:20:26 +0300

    ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Item } from './item.interface';
+import { db } from './db';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,9 @@ import { Item } from './item.interface';
 })
 export class AppComponent {
 
-  item: Item;
+  items: Item[] = [];
   constructor() {
-    this.item = {
-      title: 'Title',
-      price: 22,
-      imageUrl: 'https://slice-productimage.s3.amazonaws.com/69/df/0a/4d/47/af/43/b7/a8a7c8f181ed2f9b9190d3f7bd9f5ecbdc9b670e5b59095a'
-    };
+    this.items = db;
   }
 
 }
